196f62ad27c6cdc1eb1f4d67fc4a6a4baea3e525
jcajuab <jcajuab@gmail.com>
2025-06-25 01:56:04 +0800

    feat(server): add error handling

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,9 +2,13 @@ import { app } from "@/app";
 
 const port = Number(Bun.env.PORT ?? 3000);
 
-console.log(`Listening on http://localhost:${port}`);
+try {
+  Bun.serve({
+    fetch: app.fetch,
+    port,
+  });
 
-Bun.serve({
-  fetch: app.fetch,
-  port,
-});
+  console.log(`Listening on http://localhost:${port}`);
+} catch (error) {
+  console.error("Failed to start server", error);
+}
