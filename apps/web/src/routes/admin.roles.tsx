c16f40da813f7cd0d483ab732d0376d548908565
dpswh-dev <lanutanenzo.developer@gmail.com>
2025-07-29 17:38:27 +0800

    feat: add  header

--- apps/web/src/routes/admin.roles.tsx
+++ apps/web/src/routes/admin.roles.tsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Button } from '#/components/ui/button'
+
 export const Route = createFileRoute('/admin/roles')({
   component: Component,
 })
 
 function Component() {
-  return <h1 className='text-4xl font-bold'>Roles</h1>
+  return (
+    <>
+      {/* Header */}
+      <div className='flex items-center'>
+        <h1 className='mr-auto text-4xl font-bold'>Roles</h1>
+        <Button className='ml-auto'>+ Create Role</Button>
+      </div>
+    </>
+  )
 }
