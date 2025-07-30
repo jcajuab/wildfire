3218d353a47e91a9435f9dbaf93b583e8293c8ee
jcajuab <jcajuab@gmail.com>
2025-07-31 01:06:30 +0800

    feat(web): add reusable button-group component

---
+++ apps/web/src/components/ui/button-group.tsx
@@ -0,0 +1,32 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import type * as React from 'react'
+
+import { cn } from '#/lib/utils'
+
+const buttonGroupVariants = cva('flex items-center', {
+  variants: {
+    orientation: {
+      horizontal:
+        'flex-row *:not-first:rounded-l-none *:not-first:border-l-0 *:not-last:rounded-r-none',
+      vertical:
+        'flex-col *:not-first:rounded-t-none *:not-first:border-t-0 *:not-last:rounded-b-none',
+    },
+  },
+  defaultVariants: {
+    orientation: 'horizontal',
+  },
+})
+
+export const ButtonGroup = ({
+  className,
+  orientation,
+  ...props
+}: React.ComponentProps<'div'> & VariantProps<typeof buttonGroupVariants>) => {
+  return (
+    <div
+      className={cn(buttonGroupVariants({ orientation, className }))}
+      role='group'
+      {...props}
+    />
+  )
+}
